3b90b6917497e2d6402e571b0fa4a909178369d4
GustavoSenna07 <gustavo.psenna@gmail.com>
2025-06-03 18:50:47 -0300

    Primeira versão do DDL, coisas para rever ainda

--- Scripts/DDL.sql
+++ Scripts/DDL.sql
@@ -1,3 +1,39 @@
-create table teste(
+CREATE TABLE livro (
+    id SERIAL PRIMARY KEY,
+    FOREIGN KEY (usuario_Id) REFERENCES usuario(id),
+    titulo VARCHAR(50) NOT NULL,
+    autor VARCHAR(50) NOT NULL,
+    editora VARCHAR(50) NOT NULL,
+    ISBN VARCHAR(50) NOT NULL,
+    genero VARCHAR(30) NOT NULL,
+    ano_publicacao DATE NOT NULL,
+    quantidade_estoque INTEGER NOT NULL,
+);
 
-)g
+CREATE TABLE reserva(
+    id SERIAL PRIMARY KEY,
+    FOREIGN KEY (livro_Id) REFERENCES livro(id),
+    FOREIGN KEY (usuario_Id) REFERENCES usuario(id),
+    data_reserva DATE NOT NULL,
+    data_devolucao DATE NOT NULL,
+);
+
+CREATE TABLE emprestimo(
+	id SERIAL PRIMARY KEY,
+	--FOREIGN KEY (funcionario_Id) REFERENCES funcionario(id),
+);
+
+CREATE TABLE funcionario(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    cargo VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE usuario(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    CPF VARCHAR(14),
+    telefone VARCHAR(12),
+    endereco VARCHAR(50),
+    email VARCHAR(50)
+);
