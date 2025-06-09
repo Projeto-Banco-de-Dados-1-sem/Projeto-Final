ef16c21c00a20fe4e44b69b287a8be38a7017282
GustavoSenna07 <gustavo.psenna@gmail.com>
2025-06-09 11:16:12 -0300

    Tabelas atualizadas e tabela devolucao adicionada

--- Scripts/DDL.sql
+++ Scripts/DDL.sql
@@ -10,7 +10,8 @@ CREATE TABLE usuario (
     CPF VARCHAR(14),
     telefone VARCHAR(12),
     endereco VARCHAR(50),
-    email VARCHAR(50)
+    email VARCHAR(50),
+    data_cadastro DATE DEFAULT CURRENT_DATE -- adicionado campo opcional
 );
 
 CREATE TABLE livro (
@@ -30,6 +31,7 @@ CREATE TABLE reserva (
     usuario_id INTEGER NOT NULL,
     data_reserva DATE NOT NULL,
     data_devolucao DATE NOT NULL,
+    status VARCHAR(15) DEFAULT 'Ativa' CHECK (status IN ('Ativa', 'Cancelada', 'Finalizada')), 
     FOREIGN KEY (livro_id) REFERENCES livro(id),
     FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
@@ -41,7 +43,21 @@ CREATE TABLE emprestimo (
     funcionario_id INTEGER NOT NULL,
     data_emprestimo DATE NOT NULL,
     data_devolucao DATE NOT NULL,
+    status VARCHAR(15) DEFAULT 'Em andamento' CHECK (status IN ('Em andamento', 'Concluído', 'Atrasado')), 
     FOREIGN KEY (livro_id) REFERENCES livro(id),
     FOREIGN KEY (usuario_id) REFERENCES usuario(id),
+    FOREIGN KEY (funcionario_id) REFERENCES funcionario(id)
+);
+
+CREATE TABLE devolucao (
+    id SERIAL PRIMARY KEY,
+    emprestimo_id INTEGER NOT NULL,
+    funcionario_id INTEGER NOT NULL,
+    data_devolucao DATE NOT NULL,
+    dias_atraso INTEGER,
+    valor_multa DECIMAL(10,2),
+    multa_pago BOOLEAN DEFAULT FALSE,
+    observacao TEXT,
+    FOREIGN KEY (emprestimo_id) REFERENCES emprestimo(id),
     FOREIGN KEY (funcionario_id) REFERENCES funcionario(id)
 );
